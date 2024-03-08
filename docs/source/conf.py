13c87a49b6730fb5f9c5187d727f8431f2d2ef53
Manju <mchivuku@iu.edu>
2024-03-08 12:07:24 -0500

    updated conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,7 +5,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../src/'))
+sys.path.insert(0, os.path.abspath('../../src/simulation/'))
 
 source_suffix = {
     '.rst': 'restructuredtext',

3de2020212fa974790e55551c38098736ff4f127
Manju <mchivuku@iu.edu>
2024-03-08 12:04:23 -0500

    updated conf.py file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,16 @@
-# Configuration file for the Sphinx documentation builder.
-#
+'''Configuration file for the Sphinx documentation builder.'''
+
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../src/'))
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -9,20 +18,27 @@
 project = 'NETT Object Segmentation (ICLR 2024)'
 copyright = '2024, Manjulata Garimella'
 author = 'Manjulata Garimella'
-release = 'version 0'
+release = '0.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+  'sphinx.ext.autodoc',
+  'myst_parser',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-language = 'python'
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
+html_theme_options = {
+    "body_max_width": "none",
+}
+
 html_static_path = ['_static']
+
+html_baseurl = "/html/"
